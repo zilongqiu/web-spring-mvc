67bbb655d6d2fbeab16b22fdd6566a39a4b3962e
Zi-long QIU <zilong.qiu@gmail.com>
2022-05-29 17:46:10 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Basic Web Content with Spring MVC
+
+Exercice 14 - https://spring.io/guides/gs/serving-web-content/
+
+## Commands
+
+- `./mvnw spring-boot:run` to run
+- `./mvnw clean package` to build and then `java -jar target/spring-web-mvc-0.0.1-SNAPSHOT.jar --server.port=8080`
+- Access `http://localhost:8080/hello?name=toto`
+- or the static default page `http://localhost:8080`
---
+++ src/main/java/com/example/springwebmvc/controller/GreetingController.java
@@ -0,0 +1,18 @@
+package com.example.springwebmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+                           Model model) {
+        model.addAttribute("name", name);
+
+        return "greeting";
+    }
+}
